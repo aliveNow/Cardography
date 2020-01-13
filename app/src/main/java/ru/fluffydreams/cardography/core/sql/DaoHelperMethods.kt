52ecdd50a473b2baf1bbc038cf41c2e09a0e00b7
Mariya <samsonova.mariya@gmail.com>
2020-01-13 22:35:47 +0300

    Fix: save methods in Dao now work as insertOrUpdate instead of OnConflictStrategy.REPLACE

---
+++ app/src/main/java/ru/fluffydreams/cardography/core/sql/DaoHelperMethods.kt
@@ -0,0 +1,22 @@
+package ru.fluffydreams.cardography.core.sql
+
+inline fun <O> insertOrUpdateObject(obj: O,
+                                    insertAction: (O) -> Long,
+                                    updateAction: (O) -> Unit
+) {
+    if (insertAction(obj) == FAILED_INSERT_ID) {
+        updateAction(obj)
+    }
+}
+
+inline fun <O> insertOrUpdateList(list: List<O>,
+                                  insertAction: (List<O>) -> List<Long>,
+                                  updateAction: (List<O>) -> Unit
+) {
+    val insertResult = insertAction(list)
+    with(list.filterIndexed { index, _ -> insertResult[index] == FAILED_INSERT_ID }) {
+        if (isNotEmpty()) updateAction(this)
+    }
+}
+
+const val FAILED_INSERT_ID = -1L
--- app/src/main/java/ru/fluffydreams/cardography/datasource/local/memorize/MemorizeDao.kt
+++ app/src/main/java/ru/fluffydreams/cardography/datasource/local/memorize/MemorizeDao.kt
@@ -1,9 +1,8 @@
 package ru.fluffydreams.cardography.datasource.local.memorize
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Transaction
+import androidx.room.*
+import ru.fluffydreams.cardography.core.sql.insertOrUpdateList
+import ru.fluffydreams.cardography.core.sql.insertOrUpdateObject
 import ru.fluffydreams.cardography.datasource.local.memorize.model.AttemptEntity
 import ru.fluffydreams.cardography.datasource.local.memorize.model.AttemptResultEntity
 import ru.fluffydreams.cardography.datasource.local.memorize.model.MemFactEntity
@@ -17,13 +16,37 @@ interface MemorizeDao {
         attempts?.let { saveAttempts(it) }
     }
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun saveFact(fact: MemFactEntity)
+    @Transaction
+    fun saveFact(fact: MemFactEntity) {
+        insertOrUpdateObject(fact, ::insertFact, ::updateFact)
+    }
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun saveAttempts(attempts: List<AttemptEntity>)
+    @Transaction
+    fun saveAttempts(attempts: List<AttemptEntity>) {
+        insertOrUpdateList(attempts, ::insertAttempts, ::updateAttempts)
+    }
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun saveAttemptResults(results: List<AttemptResultEntity>)
+    @Transaction
+    fun saveAttemptResults(results: List<AttemptResultEntity>) {
+        insertOrUpdateList(results, ::insertAttemptResults, ::updateAttemptResults)
+    }
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertFact(fact: MemFactEntity): Long
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertAttempts(attempts: List<AttemptEntity>): List<Long>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertAttemptResults(results: List<AttemptResultEntity>): List<Long>
+
+    @Update
+    fun updateFact(fact: MemFactEntity)
+
+    @Update
+    fun updateAttempts(attempts: List<AttemptEntity>)
+
+    @Update
+    fun updateAttemptResults(results: List<AttemptResultEntity>)
 
 }
